afef7cf068e1b950f1bb505cd3264d8b615f2b90
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-07-28 23:18:49 +0300

    getting cryptos

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '0ae0df7644msh82af6920581187dp18e1abjsnc533541e17bd',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'    
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
