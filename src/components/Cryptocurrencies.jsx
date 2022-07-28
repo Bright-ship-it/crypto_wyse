10b06fc454909a3d7e71c2daec12e4f887edf746
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-07-29 00:21:12 +0300

    Fixing Col

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -15,7 +15,7 @@ const Cryptocurrencies = () => {
     <>
       <Row gutter={[32, 32]} className='crypto-card-container'>
         {cryptos.map((currency) => (
-          <col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
                   <Card 
                       title={`${currency.rank}. ${currency.name}`}
@@ -29,7 +29,7 @@ const Cryptocurrencies = () => {
             
             </Link>
           
-          </col>
+          </Col>
         ))}
 
       </Row>
