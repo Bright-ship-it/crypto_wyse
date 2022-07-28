13363f781ab29a5b72c6e9cbf9cb0322ee8977b8
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-07-28 22:18:36 +0300

    Done with basic page layout

--- src/App.js
+++ src/App.js
@@ -34,9 +34,17 @@ const App = () => {
 
                 </div>
             </Layout>
-        </div>
         <div className='footer'>
-
+            <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
+                CryptoWyse <br />
+                All rights reserved 2022.
+            </Typography.Title>
+            <Space>
+                <Link to='/'>Home</Link>
+                <Link to='/exchanges'>Exchanges</Link>
+                <Link to='/news'>News</Link>
+            </Space>
+        </div>
         </div>
     </div>
   );
